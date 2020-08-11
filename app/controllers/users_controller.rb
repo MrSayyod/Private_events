b556e2285ca610604081048e14817c213c570391
MrSayyod <ssayyod@gmail.com>
2020-08-11 17:21:38 +0500

    Creates 'update' method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
 
   private
   def user_params
