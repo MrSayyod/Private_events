accd4833b7ab7e338ea85b8d0c424fe39b7d50bb
MrSayyod <ssayyod@gmail.com>
2020-08-12 02:57:00 +0500

    Creates 'require_signin', 'require_correct_user' methods, adds changes to 'show' template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:index, :new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
