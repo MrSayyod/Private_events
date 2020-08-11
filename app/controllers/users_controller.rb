bb8437f2e7ede27e0e8b75d54dec0f28c3b750f0
MrSayyod <ssayyod@gmail.com>
2020-08-11 17:12:05 +0500

    Creates 'edit' method in controller and template in views/user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-    
+  
+  def edit
+    @user = User.find(params[:id])
+  end
 
   private
   def user_params
