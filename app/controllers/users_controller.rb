c9af2c0fc053c5ac0932869ed0019b875a8f0ad3
MrSayyod <ssayyod@gmail.com>
2020-08-11 17:27:19 +0500

    Creates 'destroy' method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,11 @@ class UsersController < ApplicationController
     redirect_to @user
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
   private
   def user_params
     params.require(:user).permit(:username)
