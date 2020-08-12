11b317c86390f25e7f54711716402c9bf63bc87f
MrSayyod <ssayyod@gmail.com>
2020-08-12 20:32:56 +0500

    Generates 'Attendance' model and sets association between other models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :attendances, foreign_key: "attended_event_id"
+  has_many :attendees, through: :attendances, source: :attendee
   validates :title, :description, presence: true
   validates :title, uniqueness: true
   validates :description, length: {in: 2..500}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 2 }
   has_many :events, foreign_key: "creator_id", class_name: "Event", dependent: :destroy
+  has_many :attendances, foreign_key: "attendee_id"
+  has_many :attended_events, through: :attendances, source: :attended_event
 end
