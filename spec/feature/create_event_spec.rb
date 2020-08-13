0cbfde49d19d2f9549a11923e3e0d242f52c6f36
MrSayyod <ssayyod@gmail.com>
2020-08-14 04:06:05 +0500

    Adds 'shoulda-matcher' gem into Gemfile and its configuration into rails_helper.rb, tests creating event, creates tests for user model

---
+++ spec/feature/create_event_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "Create event", type: :feature do
+  before(:each) do
+    user = User.create(username: "User")
+    visit "/session/new"
+    page.fill_in 'username', with: "User"
+    click_button "Sign In"
+  end
+
+  it "should create a new event" do
+    visit root_path
+    click_on('Create an event')
+    expect(page).to have_current_path(new_event_path)
+    page.fill_in "Title", with: "Final"
+    page.fill_in "Description", with: "UEFA Champions League final game"
+    page.fill_in "Date", with: "08/25/2020"
+    click_on("Create Event")
+    expect(page).to have_text("Congrats! A new event created!")
+  end
+end
